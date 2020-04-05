b9ebe7f67d2869be1f753f3b497e8a0043a7b288
sidharth1993 <gsaisidharth@gmail.com>
2020-04-05 19:39:29 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const Websocket = require('websocket').server;
+
+let rooms = {};
+
+const port = process.env.PORT || 4000;
+
+app.get('/',(req,res)=>{
+    res.send("Connected");
+});
+
+http.listen(port,()=>{
+    console.log(`Tuned into ${port}`)
+});
+
+const io = new Websocket({
+    httpServer : http,
+    
+});
+
+const originValidation = (origin)=>{
+    console.log(`Validating ${origin}`);
+    if(origin.indexOf('localhost') < 0){
+        return false
+    }
+    return true;
+}
+
+const validateRequest = (request)=>{
+    if(!request.resourceURL.query || (request.resourceURL.query && !request.resourceURL.query.mid)){
+        return false;
+    }
+    return true;
+}
+//rooms = {123,456,789};
+let connection = {};
+io.on("request",(request)=>{
+    if(originValidation(request.origin) && validateRequest(request)){
+        let resources = request.resourceURL;
+        const mId = resources.query.mid;
+        const user = resources.query.user;
+        if(connection[mId]){
+            connection[mId][user] = request.accept();
+        }else{
+            connection[mId] = {};
+            connection[mId][user] = request.accept();
+        }
+        
+        console.log(`Connection request Accepted for room ${mId} and user ${user}`);
+        connection[mId][user].on("message",(message)=>{
+            console.log(`Message from user ${user} to room ${mId}`);
+            for(let usr in connection[mId]){
+                connection[mId][usr].send(message.utf8Data);
+                console.log(`Sent to user ${usr} in room ${mId}`);
+            }
+        });
+        connection[mId][user].on("close",(a)=>{
+            console.log(`Connection closed with ${user} in room ${mId} --> ${a}`);
+        })
+    }else{
+        request.reject();
+    }
+
+});
+
+
+
+
